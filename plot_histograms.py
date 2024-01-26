f31cee4889ca29166195eff05e124ede0d5df015
fo-create <65010852+fo-create@users.noreply.github.com>
2024-01-26 22:28:22 +0800

    Update plot_histograms.py

--- plot_histograms.py
+++ plot_histograms.py
@@ -2,7 +2,13 @@ import torch
 from PIL import Image
 import matplotlib.pyplot as plt
 from torchvision import transforms
+这段代码通过直方图可视化了两个图像的像素值分布情况。这两个图像分别是:
 
+(a) 来自 a_gt，计算方法为 (img_tensor - (1 - mask_tensor) * gt_tensor)/(1e-8 + mask_tensor) * (mask_tensor != 0)。
+(b) 来自 za，计算方法为 img_tensor - (1 - mask_tensor) * gt_tensor。
+在直方图中，x 轴表示像素值的范围，y 轴表示该范围内像素值的数量（对数尺度）。左侧的直方图 (a) 对应 a_gt，右侧的直方图 (b) 对应 za。
+
+这样的可视化可以帮助观察图像的像素值分布，有助于了解两者之间的差异
 
 # Set up the matplotlib figure
 f, axes = plt.subplots(1, 2, figsize=(7, 7), sharex=True)
